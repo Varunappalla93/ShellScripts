1c9d2c15be26e60fcdfa0068b78be067069a5026
Varunappalla1993 <an.varun1993@gmail.com>
2026-01-28 14:19:29 +0530

    CD

--- 05_Conditions.sh
+++ 05_Conditions.sh
@@ -17,4 +17,8 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
     echo "Given Number: $NUMBER is greater than 20"
+elif [ $NUMBER -eq 20]; then
+    echo "Given Number: $NUMBER is equal to 20"
+else
+     echo "Given Number: $NUMBER is less to 20"
 fi

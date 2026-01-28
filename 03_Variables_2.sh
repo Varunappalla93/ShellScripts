210c60566da80ad2effa62b4e8c90121a80ba8ac
Varunappalla1993 <an.varun1993@gmail.com>
2026-01-28 13:46:44 +0530

    VARIABLES

--- 03_Variables_2.sh
+++ 03_Variables_2.sh
@@ -1,9 +1,9 @@
 # Day 15
 #!/bin/bash
 
-# Special Variables - can be used only inside " "
+# Special Variables - can be used only inside double quotes, i.e. " "
 # $1 $2......$N  - args passed to script via terminal 
-# $@ - All variables passed to script
+# $@ - All variables passed to script  -> $@ - treats args separately
 # $# - No of variables passed to script
 # $0 - Script Name
 # $PWD - present working directory
@@ -11,8 +11,9 @@
 # $HOME - home directory of the current user
 # $$ - PID of the script
 # $! - Background process id
+# $* - All args passed to script  -> $* - treats as single args
 
-echo "All args passed to script : $@"
+echo "All args passed to script : $@"  
 echo "No of variables passed to script : $#"
 echo "Script name: $0"
 echo "Present working directory : $PWD"
@@ -21,4 +22,16 @@ echo "Home directory of current user : $HOME"
 echo "PID of script : $$"
 sleep 100 &
 echo "PID of recently executed backgroung process : $!"
-echo "All args passed to script : $*"
+echo "All args passed to script : $*"  
+
+
+# O/P - 
+# All args passed to script : var vin
+# No of variables passed to script : 2
+# Script name: 03_Variables_2.sh
+# Present working directory : /home/ec2-user/ShellScripts
+# Who is executing the script: ec2-user
+# Home directory of current user : /home/ec2-user
+# PID of script : 2163
+# PID of recently executed backgroung process : 2164
+# All args passed to script : var vin

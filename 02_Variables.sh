a5ff6e05a25b57915c71934df32a3a98c67c9a55
Varunappalla1993 <an.varun1993@gmail.com>
2026-01-28 12:52:10 +0530

    VARIABLES

--- 02_Variables.sh
+++ 02_Variables.sh
@@ -1,12 +1,25 @@
 # Day 15
 #!/bin/bash
+
+# Variables
 # NAME="Varun"
 # AGE=32
 
 # echo "Hi ${NAME}, how are you doing ? and your age is ${AGE}" 
 
 
-# Dynamic variable arguments via terminal
-NAME=$1
-AGE=$2
-echo "Hello I am ${NAME} and my age is ${AGE}"
+# passing variable arguments via terminal
+# NAME=$1
+# AGE=$2
+# echo "Hello I am ${NAME} and my age is ${AGE}"
+
+# O/P - Hello I am QA and my age is 49
+
+# read output from terminal and assign to the variable name
+echo "Please enter your username"
+read USERNAME
+echo "username is ${USERNAME}"
+
+echo "Please enter your password"
+read PASSWORD -s
+echo "password is ${PASSWORD}"

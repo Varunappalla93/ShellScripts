aaf3f434ba989c9ed42f22d85cbaa9628d1af9f6
Varunappalla1993 <an.varun1993@gmail.com>
2026-01-28 12:47:56 +0530

    ik

--- 02_Variables.sh
+++ 02_Variables.sh
@@ -1,7 +1,12 @@
 # Day 15
-
 #!/bin/bash
-NAME="Varun"
-AGE=32
+# NAME="Varun"
+# AGE=32
 
-echo "Hi ${NAME}, how are you doing ? and your age is ${AGE}" 
+# echo "Hi ${NAME}, how are you doing ? and your age is ${AGE}" 
+
+
+# Dynamic variable arguments via terminal
+NAME=$1
+AGE=$2
+echo "Hello I am ${NAME}" and my age is ${AGE}"

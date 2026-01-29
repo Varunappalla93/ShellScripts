779afb64070c0c98123fe04d268a925cf7025fb2
Varunappalla1993 <an.varun1993@gmail.com>
2026-01-29 12:43:53 +0530

    Colors

---
+++ 09_Colors.sh
@@ -0,0 +1,45 @@
+# Day 16
+#!/bin/bash
+
+# echo -e "$RED Hello World $DEFAULT, $GREEN I am learning linux $DEFAULT"
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+# colors
+DEFAULT='\e[0m'
+RED='\e[31m'
+GREEN='\e[32m'
+BLUE='\e[33m'
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$RED Pls run this script with root user access $DEFAULT"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+# Validate function
+# tee - tee is a command that copies input to both the screen and the log file at the same time.
+VALIDATE()
+{
+if [ $1 -ne 0 ]; then
+    echo -e "Installing $2... $RED failed $NORMAL" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo -e "Installing $2... $GREEN success $NORMAL" | tee -a $LOGS_FILE
+fi
+}
+
+for package in $@    # sudo sh 08_Loops.sh nginx mysql
+do
+    dnf list installed $package &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package is not installed, installing now"
+        dnf install $package -y &>> $LOGS_FILE   
+        VALIDATE $? "$package installation"
+    else
+         echo -e "$package is already installed, $BLUE skipping $NORMAL"
+    fi
+done

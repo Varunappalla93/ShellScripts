b483262d5206447c57a08ecfba6cd03dc6a04a70
Varunappalla1993 <an.varun1993@gmail.com>
2026-02-02 15:55:16 +0530

    Shell

---
+++ 11_1_CallfromAnotherScript.sh
@@ -0,0 +1,12 @@
+# Day 18
+#!/bin/bash
+
+COUNTRY=UAE
+
+echo "I am in: $COUNTRY"
+echo "PID of script 1 is $$"
+
+
+# call another script
+# sh script2.sh
+
---
+++ 11_2_CallfromAnotherScript.sh
@@ -0,0 +1,9 @@
+# Day 18
+#!/bin/bash
+
+COUNTRY=INDIA
+
+echo "I am in: $COUNTRY"
+echo "PID of script 2 is $$"
+
+

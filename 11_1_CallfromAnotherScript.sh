cdd85cbdc459894a5b88e46614bc56b801c43c1a
Varunappalla1993 <an.varun1993@gmail.com>
2026-02-02 16:09:19 +0530

    shell

--- 11_1_CallfromAnotherScript.sh
+++ 11_1_CallfromAnotherScript.sh
@@ -6,10 +6,16 @@ COUNTRY=INDIA
 echo "I am in: $COUNTRY"
 echo "PID of script 1 is $$"
 
-# call another script
+# call another script using sh -  this will have another process id, cannot access script 1 variables.
 sh 11_2_CallfromAnotherScript.sh
 
 # I am in: INDIA
-# PID of script 1 is 14242
+# PID of script 1 is 14696
 # I am in: UAE
-# PID of script 2 is 14243
+# PID of script 2 is 14697
+
+# or use source - second script will share same process id with script 1, can access script 1 variables.
+source ./11_2_CallfromAnotherScript.sh
+
+# I am in: UAE
+# PID of script 2 is 14696

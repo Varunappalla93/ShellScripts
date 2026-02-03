eb8788ee322cb69128a1dce002200973a2af93c8
Varunappalla1993 <an.varun1993@gmail.com>
2026-02-03 18:40:44 +0530

    deletelogs

--- 12_DeleteLogs.sh
+++ 12_DeleteLogs.sh
@@ -20,8 +20,18 @@ if [ ! -d $LOGS_DIR ]; then
     exit 1
 fi
 
-FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
-echo "$FILES_TO_DELETE"
+FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -type f -mtime +14)
 
+# echo "$FILES_TO_DELETE"
+
+# O/P:
 # /home/ec2-user/applogs/shipping.log
 # /home/ec2-user/applogs/cart.log
+
+
+while IFS= read -r filepath;
+do
+   echo "Deleting file: $filepath" 
+   rm -f $filepath
+   echo "Deleted file: $filepath"
+done <<< $FILES_TO_DELETE  # <<< for inputting variable

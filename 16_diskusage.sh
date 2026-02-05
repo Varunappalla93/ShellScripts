10b2240be76a3b563db20288063a8934a6961c6d
Varunappalla1993 <an.varun1993@gmail.com>
2026-02-05 11:58:33 +0530

    Trigger mails on high disk usage

--- 16_diskusage.sh
+++ 16_diskusage.sh
@@ -9,13 +9,11 @@ RED='\e[31m'
 GREEN='\e[32m'
 BLUE='\e[33m'
 MESSAGE=""
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 log(){
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1"
 }
-
-DISK_USAGE=$(df -hT | grep -v Filesystem)
-USAGE_THRESHOLD=3
 
 while IFS= read -r line
 do
@@ -23,8 +21,11 @@ do
     PARTITION=$(echo $line | awk '{print $7}')
 
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-        MESSAGE+="High Disk usage on $PARTITION:$USAGE% \n"
+        MESSAGE+="High Disk usage on $PARTITION: $USAGE% <br>"
     fi
 done <<< $DISK_USAGE
 
 echo -e "$MESSAGE"
+
+# call mail script
+sh 17_mail.sh "info@joindevops.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
